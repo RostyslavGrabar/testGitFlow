5a38bcddf607d83e2341e4975a171834871267c8
RostyslavGrabar <rostikgrabar@gmail.com>
2020-12-17 16:58:09 +0200

    develop2

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-function authentificate (login, password){
-    if(login === 'login') {
+console.log('develop');
+function authentificate(login, password) {
+    if (login === 'login') {
+        console.log('develop');
+        
         return 'you were login'
     }
 }
+console.log('develop');
